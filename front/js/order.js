c8b91adb11bb82b2ff53b7b652bf3704058ab50d
Ahgassama <ahmad.gassama@gmail.com>
2021-11-25 18:33:40 +0100

    suppr cart log

--- front/js/order.js
+++ front/js/order.js
@@ -1,7 +1,6 @@
 import { getParamFromUrl } from "./functions.js";
 
 const orderId = getParamFromUrl("orderId");
-console.log(orderId);
 
 let getId = document.getElementById("orderId");
 getId.innerHTML = orderId;
